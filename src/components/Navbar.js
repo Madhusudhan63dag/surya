1f94f8d6f993393d5ae18434e165d6dac2fe93fb
madhusudhan123-star <dmadhusudhan98@gmail.com>
2025-05-21 16:06:28 +0530

    added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -78,7 +78,7 @@ const Navbar = () => {
           <div className="md:hidden">
             <button
               onClick={() => setIsMenuOpen(!isMenuOpen)}
-              className="inline-flex items-center justify-center p-2 rounded-md text-gray-600 hover:text-white hover:bg-gray-100 focus:outline-none"
+              className="inline-flex items-center justify-center p-2 rounded-md text-white "
               aria-expanded="false"
             >
               <span className="sr-only">Open main menu</span>
@@ -98,7 +98,7 @@ const Navbar = () => {
       </div>
 
       {/* Mobile menu */}
-      <div className={`${isMenuOpen ? 'block' : 'hidden'} md:hidden bg-[#f26522] shadow-lg`}>
+      <div className={`${isMenuOpen ? 'block' : 'hidden'} md:hidden bg-[#002E62] shadow-lg`}>
         <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
           <div onClick={() => scrollToSection('home')} className="block px-3 py-2 text-white hover:text-white hover:bg-gray-50 rounded-md transition-colors duration-300 cursor-pointer">
             Home
--- src/components/One.js
+++ src/components/One.js
@@ -31,8 +31,9 @@ const One = () => {
   return (
     <div className='overflow-hidden'>  
       <div id='home'>
-        <h1 className='text-5xl sm:text-5xl md:text-6xl lg:text-[7rem] relative bottom-0 lg:-bottom-2 md:-bottom-4 font-bold text-[#f26522] mt-0 pt-0 py-5 md:py-0 px-4 text-left'>SURYA MEDIA</h1>
-        <div className='h-[70vh] sm:h-[40vh] md:h-[80vh] w-screen'>
+        <h1 className='text-5xl sm:text-5xl md:text-6xl lg:text-[7rem] relative bottom-0 lg:-bottom-2 md:-bottom-4 font-bold text-[#2566b0] mt-0 pt-0 py-5 md:py-0 px-4 text-left'>SURYA MEDIA</h1>
+        <div className='h-[70vh] sm:h-[40vh] md:h-[80vh] w-screen relative'>
+          <div className='absolute z-10 inset-0 bg-black opacity-20'></div>
           {videoSrc && (
             <OptimizedBackgroundVideo
               src={videoSrc}
@@ -47,14 +48,14 @@ const One = () => {
                 width: '100%'
               }}
               videoStyle={{
-                objectFit: 'cover'
+                objectFit: isMobile ? '' : 'cover' // Explicitly set 'cover' for mobile screens
               }}
             />
           )}
         </div>
       </div>
-      <div className='flex flex-col items-end justify-items-end px-4 sm:px-0'>
-        <h1 className='text-4xl sm:text-3xl md:text-4xl lg:text-[4rem] font-bold text-[#f26522] md:mb-5 mb-0 mr-0 sm:mr-10 md:mr-20 lg:mr-40 text-right'>One Agency. Infinite</h1>
+      <div className='flex flex-col items-end justify-items-end px-4 pt-3 sm:px-0'>
+        <h1 className='text-4xl sm:text-3xl md:text-4xl lg:text-[4rem] font-bold text-[#2566b0] md:mb-5 mb-0 mr-0 sm:mr-10 md:mr-20 lg:mr-40 text-right'>One Agency. Infinite</h1>
         <h1 className='text-5xl sm:text-3xl md:text-4xl lg:text-[4rem] font-bold text-[#b0b0b0] md:mb-5 mb-0 text-right'>Possibilities</h1>
       </div>
       <h3 className='text-lg aligh sm:text-base font-bold md:text-lg mt-10 lg:text-[2rem] text-[2c1a47]'>From UGC to digital marketing <br/> we bring your vision to life.</h3>
